f2c52efcf1be46331d61067eea6e8acb6dffcc53
Kargnin <bhushanmalani3@iitkgp.ac.in>
2023-02-01 00:20:44 +0530

    docs: :memo: Added documentation for client library

--- sdk/disqueue/consumer.py
+++ sdk/disqueue/consumer.py
@@ -3,6 +3,12 @@ import requests as req
 
 class Consumer:
     def __init__(self, topics: list[str], broker: str):
+        """Constructor for Consumer class
+
+        Args:
+            topics (list[str]): List of topics to be consumed
+            broker (str): url of the broker
+        """
         # Check if / is present at the end of broker
         if broker[-1] == '/':
             broker = broker[:-1] # Remove the last character
@@ -13,16 +19,34 @@ class Consumer:
             self.register_topic(topic)
 
     def register_topic(self, topic: str) -> None:
+        """Function to register a topic
+
+        Args:
+            topic (str): topic to be registered
+
+        Raises:
+            Exception: If the response is not ok
+        """
         res = req.post(self.broker + '/consumer/register',
-                       json={'topic': topic})
+                       params={'topic': topic})
         if not res.ok:
             raise Exception(message=res.json()['detail'])
         self.topic_cons_ids[topic] = res.json()['consumer_id']
         print(res.json())
 
     def get_next(self, topic: str) -> str:
+        """
+        Function to get the next message from the topic
+        
+        Args:
+            topic (str): topic to be consumed
+        Return:
+            str: message from the topic
+        Raises:
+            Exception: If the response is not ok
+        """
         res = req.get(self.broker + '/consumer/consume',
-                       json={'topic': topic,
+                       params={'topic': topic,
                              'consumer_id': self.topic_cons_ids[topic]})
         if not res.ok:
             raise Exception(message=res.json()['detail'])
@@ -30,8 +54,19 @@ class Consumer:
         return res.json()['message']
     
     def get_size(self, topic: str) -> int:
+        """Function to get the size of the topic
+
+        Args:
+            topic (str): topic to be consumed
+            
+        Raises:
+            Exception: If the response is not ok
+
+        Returns:
+            int: size of the topic
+        """
         res = req.get(self.broker + '/size',
-                       json={'topic': topic,
+                       params={'topic': topic,
                              'consumer_id': self.topic_cons_ids[topic]})
         if not res.ok:
             raise Exception(message=res.json()['detail'])
--- sdk/disqueue/producer.py
+++ sdk/disqueue/producer.py
@@ -3,6 +3,12 @@ import requests as req
 
 class Producer:
     def __init__(self, topics: list[str], broker: str):
+        """Constructor for Producer class   
+
+        Args:
+            topics (list[str]): List of topics to be produced
+            broker (str): url of the broker
+        """
         # Check if / is present at the end of broker
         if broker[-1] == '/':
             broker = broker[:-1]  # Remove the last character
@@ -13,18 +19,39 @@ class Producer:
             self.register_topic(topic)
 
     def register_topic(self, topic: str) -> int:
+        """Function to register a topic
+
+        Args:
+            topic (str): topic to be registered
+
+        Raises:
+            Exception: If the response is not ok
+
+        Returns:
+            int: producer_id
+        """
         res = req.post(self.broker + '/producer/register',
-                       json={'topic': topic})
+                       params={'topic': topic})
         if not res.ok:
             raise Exception(message=res.json()['detail'])
         self.topic_prod_ids[topic] = res.json()['producer_id']
         print(res.json())
 
     def send_message(self, topic: str, message: str):
+        """Function to send a message to a topic
+
+        Args:
+            topic (str): topic to be produced
+            message (str): message to be sent
+
+        Raises:
+            Exception: If the response is not ok
+            Exception: If the topic is not registered
+        """
         if topic not in self.topic_prod_ids:
             raise Exception(message="No topic found")
         res = req.post(self.broker + '/producer/produce',
-                       json={'topic': topic,
+                       params={'topic': topic,
                              "producer_id": self.topic_prod_ids[topic],
                              "message": message
                              }
@@ -32,4 +59,3 @@ class Producer:
         if not res.ok:
             raise Exception(message=res.json()['edtail'])
         print(res.json())
-    ...
