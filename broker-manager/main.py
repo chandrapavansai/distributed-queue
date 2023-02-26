c59ad107b0b08d3a3ef709834a8ac6a67bdbf33a
Sri Ram Chowdary Ravi <rsriramc@gmail.com>
2023-02-26 13:07:03 +0530

    Added Broker Manager Boilerplate

---
+++ broker-manager/main.py
@@ -0,0 +1,144 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Run the Leader Selection Algorithm
+
+"""
+Manage the data:
+1. Clients:
+    - Current Parition w.r.t to the topic - For Round Robin
+    - Last fetched offset
+    - Alive or not
+
+2. Brokers:
+    - List of live brokers
+    - Hash table of topic parition to broker mapping
+"""
+
+"""
+API Calls:
+1. Leader
+    - New Broker
+    - New Partition
+    - Creation of topic
+    - Register of Producer, Consumer to a topic - /producer/register, /consumer/register
+    - Consumer Dequeue - Get the message from the queue (Offset change) - /consumer/consume
+
+2. Follower - Any API call which does not change the metadata of the system
+    - Producer Enqueue - Add the message to the queue - /producer/produce
+    - Get size of the queue - /size
+    - Heartbeat - /heartbeat
+    - Data Synchronization - /sync
+"""
+
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
+
+@app.get("/topics")
+@app.get("/topics/paritions")
+def list_topics():
+    """
+    Endpoint to list all topics and paritions
+    :return: list of topics and paritions
+    """
+    return {"topics": {
+        "topic1": [1, 2],
+        "topic2": [1, 2, 3]
+    }}
+    pass
+
+
+@app.post("/topics")
+def create_topic(name: str):
+    """
+    Endpoint to create a topic
+    :param name: name of the topic
+    :return: success message
+    """
+    pass
+
+
+@app.post("/topics/paritions")
+def create_parition(topic: str, parition: int):
+    """
+    Endpoint to create a parition for a topic
+    :param topic: name of the topic
+    :param parition: parition number
+    :return: success message
+    """
+    pass
+
+
+@app.post("/consumer/register")
+def register_consumer(topic: str):
+    """
+    Endpoint to register a consumer for a topic
+    :param topic: the topic to which the consumer wants to subscribe
+    :return: consumer id
+    """
+    pass
+
+
+@app.post("/producer/register")
+def register_producer(topic: str):
+    """
+    Endpoint to register a producer for a topic
+    :param topic: the topic to which the producer wants to publish
+    :return: producer id
+    """
+    pass
+
+
+@app.get("/consumer/consume")
+def dequeue(topic: str, consumer_id: str):
+    """
+    Endpoint to dequeue a message from the queue
+    :param topic: the topic from which the consumer wants to dequeue
+    :param consumer_id: consumer id obtained while registering
+    :return: log message
+    """
+    pass
+
+
+@app.post("/producer/produce")
+def enqueue(topic: str, producer_id: str, message: str):
+    """
+    Endpoint to enqueue a message to the queue
+    :param topic: the topic to which the producer wants to enqueue
+    :param producer_id: producer id obtained while registering
+    :param message: log message to be enqueued
+    """
+    pass
+
+
+@app.get("/size")
+async def size(topic: str, consumer_id: str):
+    """
+    Endpoint to get the size of the queue for a given topic
+    :param topic: the topic for which the size is to be obtained
+    :param consumer_id: consumer id obtained while registering
+    :return: size of the queue
+    """
+    pass
+
+
+@app.get("/heartbeat")
+def heartbeat():
+    """
+    Endpoint to send heartbeat
+    :return: success message
+    """
+    pass
+
+
+@app.get("/sync")
+def sync():
+    """
+    Endpoint to sync data
+    :return: success message
+    """
+    pass
