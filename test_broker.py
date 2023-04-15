ca2bfc67dde2e5d2a1744f67d009bed81a30b87c
Kargnin <bhushanmalani3@iitkgp.ac.in>
2023-04-15 13:55:41 +0530

    test: :white_check_mark: Updated broker tests

--- test_broker.py
+++ test_broker.py
@@ -6,10 +6,10 @@ class TestBroker:
     def __init__(self):
         self.broker1_url = "http://localhost:8000"
         self.broker2_url = "http://localhost:8001"
+        self.broker3_url = "http://localhost:8002"
     
     def assertEqual(self, a, b):
         assert a == b
-
 
     def test_ping(self):
         response = requests.get(f"{self.broker1_url}/ping")
@@ -21,6 +21,11 @@ class TestBroker:
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "pong"})
 
+        # Test 3rd broker
+        response = requests.get(f"{self.broker3_url}/ping")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "pong"})
+
     def test_create_message(self):
         # Delete the topic-partition if it exists
         response = requests.get(f"{self.broker1_url}/messages/delete", params={
@@ -29,28 +34,19 @@ class TestBroker:
         })
         self.assertEqual(response.status_code, 200)
 
-        # Create topic partition
-        response = requests.post(f"{self.broker1_url}/new", params={
+        # json inside body
+        request = requests.post(f"{self.broker1_url}/new", json={
             "topic": "test",
             "partition": 1,
-            "partners": "raft-broker2:9000,raft-broker1:9000"
+            "partners": ["raft-broker1:9000"]
         })
-        self.assertEqual(response.status_code, 200)
-
-        # Create topic partition
-        response = requests.post(f"{self.broker2_url}/new", params={
-            "topic": "test",
-            "partition": 1,
-            "partners": "raft-broker2:9000,raft-broker1:9000"
-        })
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(request.status_code, 200)
 
         # Create a message on broker 1
-        response = requests.post(f"{self.broker1_url}/messages", params={
+        response = requests.post(f"{self.broker1_url}/messages", json={
             "topic": "test",
             "partition": 1,
             "content": "test message",
-            "partners": "raft-broker2:9000,raft-broker1:9000"
         })
         self.assertEqual(response.status_code, 201)
 
@@ -73,27 +69,18 @@ class TestBroker:
         self.assertEqual(response.status_code, 200)
 
         # Create topic partition
-        response = requests.post(f"{self.broker1_url}/new", params={
+        response = requests.post(f"{self.broker1_url}/new", json={
             "topic": "test",
             "partition": 1,
-            "partners": "raft-broker2:9000,raft-broker1:9000"
-        })
-        self.assertEqual(response.status_code, 200)
-
-        # Create topic partition
-        response = requests.post(f"{self.broker2_url}/new", params={
-            "topic": "test",
-            "partition": 1,
-            "partners": "raft-broker2:9000,raft-broker1:9000"
+            "partners": ["raft-broker1:9000"]
         })
         self.assertEqual(response.status_code, 200)
 
         # Create a message on broker 1
-        response = requests.post(f"{self.broker1_url}/messages", params={
+        response = requests.post(f"{self.broker1_url}/messages", json={
             "topic": "test",
             "partition": 1,
             "content": "test message",
-            "partners": "raft-broker2:9000,raft-broker1:9000"
         })
         self.assertEqual(response.status_code, 201)
 
@@ -106,42 +93,36 @@ class TestBroker:
 
     # Create new topic-partition
     def test_create_topic_partition(self):
-        print("Sleeping")
-        time.sleep(1)
-        print("Awake")
         # Delete the topic-partition if it exists
         response = requests.get(f"{self.broker1_url}/messages/delete", params={
             "topic": "test",
             "partition": 2,
         })
         self.assertEqual(response.status_code, 200)
-        time.sleep(1)
-        # # Create topic partition
-        # response = requests.post(f"{self.broker1_url}/new", params={
-        #     "topic": "test",
-        #     "partition": 2,
-        #     "partners": "raft-broker2:9000,raft-broker1:9000"
-        # })
-        # self.assertEqual(response.status_code, 200)
-
-        # Paralelly send two requests to create the same topic-partition
-        # Create a new thread
-        # t1 = threading.Thread(target=self.create_topic_partition, args=(self.broker1_url, "test", 2, "raft-broker2:9000,raft-broker1:9000"))
 
         # Create topic partition
-        response = requests.post(f"{self.broker2_url}/new", params={
+        response = requests.post(f"{self.broker1_url}/new", json={
             "topic": "test",
             "partition": 2,
-            "partners": "raft-broker2:9000,raft-broker1:9000"
+            "partners": ["raft-broker2:9000","raft-broker1:9000"]
         })
         self.assertEqual(response.status_code, 200)
 
+        # Create topic partition
+        response = requests.post(f"{self.broker2_url}/new", json={
+            "topic": "test",
+            "partition": 2,
+            "partners": ["raft-broker2:9000","raft-broker1:9000"]
+        })
+        self.assertEqual(response.status_code, 200)
+
+        time.sleep(1)
+
         # Create a message on broker 1
-        response = requests.post(f"{self.broker1_url}/messages", params={
+        response = requests.post(f"{self.broker1_url}/messages", json={
             "topic": "test",
             "partition": 2,
             "content": "test message",
-            "partners": "raft-broker2:9000,raft-broker1:9000"
         })
         self.assertEqual(response.status_code, 201)
 
